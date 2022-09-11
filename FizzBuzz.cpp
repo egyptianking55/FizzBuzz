f07c5d5c607bf617549bdab36358965c1463f174
Egyptianking55 <egyptianking55@gmail.com>
2022-09-10 18:14:42 -0700

    Updated optional output.

--- FizzBuzz.cpp
+++ FizzBuzz.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main () {
-
+//initialized variables.  start count at one, 0 false positive
     int counter = 1;
     int end;
+    string fizz, buzz;
 
     cout << "How far do you want to count?";
 
+//determine end of count
     cin >> end;
 
+    cout << "What is your fizz? ";
+    cin >> fizz;
+
+    cout << "\nWhat is your buzz? ";
+    cin >> buzz;
+    cout << "\n";
+
+//standard fizzbuzz logic loop
     while (counter <= end) {
 
         if (counter % 15 == 0) {
-            cout << "fizzbuzz";
+            cout << fizz << buzz;
         }
         else if (counter % 5 == 0) {
-            cout << "buzz";
+            cout << buzz;
         }
         else if (counter % 3 == 0) {
-            cout << "fizz";
+            cout << fizz;
         }
         else {
             cout << counter;
